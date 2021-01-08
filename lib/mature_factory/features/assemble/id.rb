cca8aa774f39e05f11cabfb8caa844cb5bdc19c6
Andrii Baran <andriy.baran.v@gmail.com>
2021-01-08 14:02:47 +0200

    Improved Id object to_s method

--- lib/mature_factory/features/assemble/id.rb
+++ lib/mature_factory/features/assemble/id.rb
@@ -1,7 +1,7 @@
 module MatureFactory::Features::Assemble
   class Id < Struct.new(:group, :title)
     def to_s
-      to_a.reverse.join('_')
+      to_a.compact.reverse.join('_')
     end
 
     def to_sym
